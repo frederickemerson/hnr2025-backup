9ea66777cb65baeab5084783b6b516500e06c35e
Raymond Goo <raymond.goo124@gmail.com>
2025-01-19 05:56:18 +0800

    feat: for loop leaderboard route.

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -1,6 +1,50 @@
+"use client"
 import Image from "next/image";
+import {useEffect, useState} from "react";
 
 const Leaderboard = () => {
+
+  const [leaderboard, setLeaderboard] = useState<any[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
+
+  useEffect(() => {
+    fetch('/api/score')
+      .then(res => res.json())
+      .then(data => {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+        setLeaderboard(data);
+        setIsLoading(false);
+      })
+      .catch(err => {
+        console.error('Failed to fetch leaderboard data:', err);
+        setHasError(true);
+        setIsLoading(false);
+      });
+  }, []);
+
+  const renderLeaderboard = () => {
+    if (isLoading) {
+      return <p className="text-center text-2xl">Loading...</p>;
+    }
+    if (hasError) {
+      return <p className="text-center text-2xl">Error fetching data.</p>;
+    }
+    if (leaderboard.length === 0) {
+      return <p className="text-center text-2xl">No users in leaderboard.</p>;
+    }
+    return (
+      <ul className="list-decimal pl-5">
+        {leaderboard.map((user, index) => {
+          return (
+            <li key={user.id} className="text-xl">
+              {user.name} - TIme
+            </li>
+          )
+        })}
+      </ul>
+    );
+  };
   return (
 
     <div className="relative min-h-screen w-full">
@@ -23,7 +67,7 @@ const Leaderboard = () => {
             <div className="p-10 bg-gray-200">
               <h1 className="text-3xl font-bold">Leaderboard</h1>
               <div>
-
+                {renderLeaderboard()}
               </div>
             </div>
           </div>
