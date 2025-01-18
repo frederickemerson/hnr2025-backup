94eadcd31b58a11a563b3e71db7aac16e77e32a9
Derrick Wong Liang Jun <derrickwong8909@gmail.com>
2025-01-18 16:04:42 +0800

    Display all levels

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import LevelOne from "./LevelOne";
+import LevelTwo from "./LevelTwo";
+import LevelThree from "./LevelThree";
 import Counter from "./Counter";
 import React, { useState } from "react";
 
 export default function HomePage() {
   const [showLevelOne, setShowLevelOne] = useState<boolean>(false);
+  const [showLevelTwo, setShowLevelTwo] = useState<boolean>(false);
+  const [showLevelThree, setShowLevelThree] = useState<boolean>(false);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
@@ -17,7 +21,21 @@ export default function HomePage() {
         >
           {showLevelOne ? "Hide Level One" : "Show Level One"}
         </button>
+        <button
+          onClick={() => setShowLevelTwo((prev) => !prev)}
+          className="rounded-xl bg-blue-500 px-6 py-3 font-bold text-white shadow-md transition-all hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300"
+        >
+          {showLevelTwo ? "Hide Level Two" : "Show Level Two"}
+        </button>
+        <button
+          onClick={() => setShowLevelThree((prev) => !prev)}
+          className="rounded-xl bg-blue-500 px-6 py-3 font-bold text-white shadow-md transition-all hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300"
+        >
+          {showLevelThree ? "Hide Level Three" : "Show Level Three"}
+        </button>
         {showLevelOne && <LevelOne />}
+        {showLevelTwo && <LevelTwo />}
+        {showLevelThree && <LevelThree />}
       </div>
     </main>
   );
