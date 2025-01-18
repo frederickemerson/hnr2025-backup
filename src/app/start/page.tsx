d1c54484958326a28c88c7c1c1b9a40c5178923f
Derrick Wong Liang Jun <derrickwong8909@gmail.com>
2025-01-18 18:57:21 +0800

    Include level Four

--- src/app/start/page.tsx
+++ src/app/start/page.tsx
@@ -3,6 +3,7 @@
 import LevelOne from "../components/LevelOne";
 import LevelTwo from "../components/LevelTwo";
 import LevelThree from "../components/LevelThree";
+import LevelFour from "../components/LevelFour";
 import Counter from "../components/Counter";
 import React, { useState } from "react";
 
@@ -10,6 +11,7 @@ export default function HomePage() {
   const [showLevelOne, setShowLevelOne] = useState<boolean>(false);
   const [showLevelTwo, setShowLevelTwo] = useState<boolean>(false);
   const [showLevelThree, setShowLevelThree] = useState<boolean>(false);
+  const [showLevelFour, setShowLevelFour] = useState<boolean>(false);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
@@ -33,9 +35,16 @@ export default function HomePage() {
         >
           {showLevelThree ? "Hide Level Three" : "Show Level Three"}
         </button>
+        <button
+          onClick={() => setShowLevelFour((prev) => !prev)}
+          className="rounded-xl bg-blue-500 px-6 py-3 font-bold text-white shadow-md transition-all hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300"
+        >
+          {showLevelFour ? "Hide Level Four" : "Show Level Four"}
+        </button>
         {showLevelOne && <LevelOne />}
         {showLevelTwo && <LevelTwo />}
         {showLevelThree && <LevelThree />}
+        {showLevelFour && <LevelFour />}
       </div>
     </main>
   );
